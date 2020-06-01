018fa6c49970a81b0a1bb186ca468acd48ede001
icorrea <italiviocorrea@gmail.com>
2020-06-01 07:23:32 -0400

    Removendo projeto scala

--- api-ncm-reactive/src/main/scala/eti/italiviocorrea/ncm/Ncm.scala
+++
@@ -1,6 +0,0 @@
-package eti.italiviocorrea.ncm
-
-import java.util.Date
-
-case class Ncm(id: String, nome: String, inicioVigencia: Date, fimVigencia: Date, utribCodigo: String, utribDescricao: String)
-
