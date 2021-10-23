983aaae26a90ee08059bfed603ced2a2cb1f884b
icorrea <italiviocorrea@gmail.com>
2021-10-23 16:52:22 -0400

    Implementando serializable

--- api-lcr-rsocket/src/main/java/br/gov/ms/sefaz/sat/cotin/dfe/api/lcr/application/domain/AutoridadeCertificadora.java
+++ api-lcr-rsocket/src/main/java/br/gov/ms/sefaz/sat/cotin/dfe/api/lcr/application/domain/AutoridadeCertificadora.java
@@ -2,12 +2,14 @@ package br.gov.ms.sefaz.sat.cotin.dfe.api.lcr.application.domain;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Data
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class AutoridadeCertificadora {
+public class AutoridadeCertificadora implements Serializable {
 
     Integer iditc;
     String alias;
--- api-lcr-rsocket/src/main/java/br/gov/ms/sefaz/sat/cotin/dfe/api/lcr/application/domain/ListaCertificadoRevogado.java
+++ api-lcr-rsocket/src/main/java/br/gov/ms/sefaz/sat/cotin/dfe/api/lcr/application/domain/ListaCertificadoRevogado.java
@@ -2,12 +2,14 @@ package br.gov.ms.sefaz.sat.cotin.dfe.api.lcr.application.domain;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Data
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ListaCertificadoRevogado {
+public class ListaCertificadoRevogado implements Serializable {
 
     Integer id;
     int indiLcrDelta;
